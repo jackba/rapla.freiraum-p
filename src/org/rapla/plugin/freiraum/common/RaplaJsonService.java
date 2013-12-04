66904f6b2a18ee8f84a00c3f5d2d74a9d37fa0df
Christopher Kohlhaas <christopher.kohlhaas@gmail.com>
2013-12-04 20:04:19 +0100

    allow crosssite

--- src/org/rapla/plugin/freiraum/common/RaplaJsonService.java
+++ src/org/rapla/plugin/freiraum/common/RaplaJsonService.java
@@ -2,15 +2,18 @@ package org.rapla.plugin.freiraum.common;
 
 import java.util.List;
 
+import com.google.gwtjsonrpc.common.AllowCrossSiteRequest;
 import com.google.gwtjsonrpc.common.AsyncCallback;
 
 public interface RaplaJsonService extends com.google.gwtjsonrpc.common.RemoteJsonService
 {
+	@AllowCrossSiteRequest
 	void getResources(String type,String searchTerm,AsyncCallback<List<ResourceDescriptor>> callback);
-	
+	@AllowCrossSiteRequest
 	void getResource(String id,AsyncCallback<ResourceDetail> callback);
-	
+	@AllowCrossSiteRequest
 	void getOrganigram(AsyncCallback<CategoryDescription> callback);
-	
+	@AllowCrossSiteRequest
 	void getResourcesFromCategory(String type,CategoryDescription category, AsyncCallback<List<ResourceDescriptor>> callback);
+	
 }
